cd9e387e8969674d9ae309d8cbbae9589426b6e8
Momo Bara <Momobaraa76@gmail.com>
2021-07-08 16:24:41 -0700

    new update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,66 @@
+const taskName = document.getElementById('name');
+const description = document.getElementById('description');
+const assignedTo = document.getElementById('select');
+const dueDate = document.getElementById('dueDate');
+const status = document.getElementById('status');
+const submit = document.getElementById('send');
+const cardDiv = document.getElementById('jsCard');
 
-const task = new TaskManager(1);
-task.addTask('Laz', 'create a website', 'Momo', '7/10/2021');
+submit.addEventListener('click', function() {
+   const task = new TaskManager();
+    task.addTask(taskName.value, description.value, assignedTo.value, dueDate.value, status.value);
+    console.log(task.tasks)
+    //console.log(dueDate.value);
+
+    let newDiv = document.createElement("div");
+
+    newDiv.innerHTML = `<div class="card col-lg-4 col-md-6 col-sm-12 px-0">
+    <div class="row cardimage">
+    <img
+      src="../ProjectOne/images/istockphoto-1153700372-612x612.jpg"
+      class="card-img-top "
+      alt="..."
+    />
+  </div>
+    <div class="card-header bg-secondary row justify-content-center">
+      <h5 class="card-title cardtitle">${task.tasks[0].name}</h5>
+    </div>
+    <div class="card-body cardgreen row justify-content-center my-0 py-0">
+    <ul class="list-group list-group-flush ">
+      <li class="list-group-item cardgreen border-0 " >${task.tasks[0].name}</li>
+      <li class="list-group-item cardgreen border-0  ">Description : ${task.tasks[0].description} </li>
+      <li class="list-group-item cardgreen border-0  ">AssignedTo : ${task.tasks[0].assignedTo}</li>
+      <li class="list-group-item cardgreen border-0 ">Due Date : ${task.tasks[0].dueDate}</li>
+      <li class="list-group-item cardgreen border-0 ">Status : ${task.tasks[0].status}</li>
+    </ul>
+  </div>
+    <div class="card-footer bg-secondary row ">
+      <div class="col">
+      <input type="checkbox" class="btn-check " id="btn-check-1" checked autocomplete="off">
+      <label class="btn btn-light  " for="btn-check-1">Done</label>
+    </div>
+    <div class="col ">
+      <input type="checkbox" class="btn-check" id="btn-check-2" checked autocomplete="off">
+      <label class="btn btn-light" for="btn-check-2">Delete</label>
+    </div>
+     <!-- <a href="#" class="card-link text-left">Done</a> -->
+     <!-- <a href="#" class="card-link text-left">Delete</a> -->
+    </div>
+    </div>`
+
+    cardDiv.append(newDiv);
+});
+
+
+
+
+
+
+
+
+/*
+const task = new TaskManager(0);
 task.addTask('Laz', 'create a website', 'Momo', '7/10/2021');
 task.addTask('Laz', 'create a website', 'Momo', '7/10/2021');
 console.log(task.tasks);
+*/
